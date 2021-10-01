f3e37f50a45b026ed5f96784a4a7833dcd2298e4
tyfahpundo <tafadzwaallentyfah@gmail.com>
2021-10-01 09:15:18 +0200

    Initial Commit

---
+++ src/main/java/zw/co/afrosoft/CrudBestPracticeApplication.java
@@ -0,0 +1,13 @@
+package zw.co.afrosoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudBestPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudBestPracticeApplication.class, args);
+    }
+
+}
